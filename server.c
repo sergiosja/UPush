21d4e74ad22a55bd999d78b2752f76c3cb491009
Sergey <thefanthomasfan@gmail.com>
2022-06-16 18:14:45 +0200

    refactor server

--- server.c
+++ server.c
@@ -23,36 +23,36 @@ int main(int argc, char const **argv)
 
 	int rc = bind(fd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr_in));
 	check_perror(rc, "Socket reserved");
-    printf("> Server running on port %s...\n", argv[1]);
+	printf("> Server running on port %s...\n", argv[1]);
 	for (;;)
 	{
         char **request = receive_message(fd, &client_addr);
 
-		int current_client_port = ntohs(client_addr.sin_port);
-		char current_client_address[INET_ADDRSTRLEN];
-		inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, current_client_address, INET_ADDRSTRLEN);
+        int current_client_port = ntohs(client_addr.sin_port);
+        char current_client_address[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, current_client_address, INET_ADDRSTRLEN);
 
-		// Registers new client
-		if (!strcmp(request[2], "REG"))
-		{
-			client *existing_client = find_client_by_nick(cache, request[3]);
-			gettimeofday(&registration_time, NULL);
-			int current_time = registration_time.tv_sec;
+        // Registers new client
+        if (!strcmp(request[2], "REG"))
+        {
+            client *existing_client = find_client_by_nick(cache, request[3]);
+            gettimeofday(&registration_time, NULL);
+            int current_time = registration_time.tv_sec;
             char heartbeat = '0';
 
-			if (existing_client)
+            if (existing_client)
             {
                 if (!strcmp(existing_client->address, current_client_address) && (existing_client->port == current_client_port))
-                    heartbeat = '1';
+                heartbeat = '1';
                 update_client(existing_client, current_client_address, current_client_port, current_time);
             }
-			else register_client(cache, request[3], current_client_address, current_client_port, current_time);
+            else register_client(cache, request[3], current_client_address, current_client_port, current_time);
             if (heartbeat == '0') send_ack_ok(fd, client_addr, request[1][0]);
-		}
+        }
 
-		// Looks up existing client
-		else if (!strcmp(request[2], "LOOKUP"))
-		{
+        // Looks up existing client
+        else if (!strcmp(request[2], "LOOKUP"))
+        {
             if (find_client_by_address(cache, current_client_address, current_client_port))
             {
                 client *existing_client = find_client_by_nick(cache, request[3]);
@@ -68,7 +68,7 @@ int main(int argc, char const **argv)
                 if (!existing_client) send_ack_notfound(fd, client_addr, request[1][0]);
                 else send_client_info(fd, client_addr, request[1][0], existing_client);
             }
-		}
+        }
         free_request(request);
 	}
 
@@ -80,18 +80,18 @@ int main(int argc, char const **argv)
 
 client *initiate_client_register()
 {
-    client *head = malloc(sizeof(client));
-    head->nick = NULL;
-    head->address = NULL;
-    head->next = NULL;
-    return head;
+	client *head = malloc(sizeof(client));
+	head->nick = NULL;
+	head->address = NULL;
+	head->next = NULL;
+	return head;
 }
 
 
 void register_client(client *head, char *nick, char *address, uint16_t port, long registered)
 {
 	client *new_client = malloc(sizeof(client));
-    new_client->nick = strdup(nick);
+	new_client->nick = strdup(nick);
 	new_client->address = strdup(address);
 	new_client->port = port;
 	new_client->last_registered = registered;
@@ -103,7 +103,7 @@ void register_client(client *head, char *nick, char *address, uint16_t port, lon
 
 	client *current = head;
 	while (current->next)
-		current = current->next;
+    	current = current->next;
 	current->next = new_client;
 	new_client->next = NULL;
 }
@@ -138,14 +138,14 @@ client *find_client_by_nick(client *head, char *nick)
 
 client *find_client_by_address(client *head, char *address, uint16_t port)
 {
-    client *current = head->next;
-    while (current)
-    {
-        if (!strcmp(current->address, address) && (current->port == port))
-            return current;
-        current = current->next;
-    }
-    return NULL;
+	client *current = head->next;
+	while (current)
+	{
+		if (!strcmp(current->address, address) && (current->port == port))
+			return current;
+		current = current->next;
+	}
+	return NULL;
 }
 
 
@@ -196,19 +196,19 @@ char** deserealise_message(char *message)
 
 
 char **receive_message(int fd, struct sockaddr_in *address) {
-    char buffer[BUFSIZE];
-    socklen_t addr_len = sizeof(struct sockaddr_in);
-    int rc = recvfrom(fd, buffer, BUFSIZE-1, 0, (struct sockaddr *) address, &addr_len);
-    check_perror(rc, "recvfrom");
-    buffer[rc] = 0;
-    return deserealise_message(buffer);
+	char buffer[BUFSIZE];
+	socklen_t addr_len = sizeof(struct sockaddr_in);
+	int rc = recvfrom(fd, buffer, BUFSIZE-1, 0, (struct sockaddr *) address, &addr_len);
+	check_perror(rc, "recvfrom");
+	buffer[rc] = 0;
+	return deserealise_message(buffer);
 }
 
 
 void free_request(char **request)
 {
-    for (int i = 0; i < 4; i++)
-        free(request[i]);
+	for (int i = 0; i < 4; i++)
+	free(request[i]);
 }
 
 
@@ -226,12 +226,12 @@ int get_port_length(int port)
 
 void send_ack_ok(int fd, struct sockaddr_in address, char number)
 {
-    char *message = malloc(ACK_OK_SIZE);
+	char *message = malloc(ACK_OK_SIZE);
 	snprintf(message, ACK_OK_SIZE, "ACK %c OK", number);
-    int wc = send_packet(fd, message, strlen(message), 0,
-                    (struct sockaddr *) &address, sizeof(struct sockaddr_in));
-    check_perror(wc, "send_packet");
-    free(message);
+	int wc = send_packet(fd, message, strlen(message), 0,
+						(struct sockaddr *) &address, sizeof(struct sockaddr_in));
+	check_perror(wc, "send_packet");
+	free(message);
 }
 
 
@@ -239,43 +239,47 @@ void send_ack_notfound(int fd, struct sockaddr_in address, char number)
 {
 	char *message = malloc(ACK_NOT_FOUND_SIZE);
 	snprintf(message, ACK_NOT_FOUND_SIZE, "ACK %c NOT FOUND", number);
-    int wc = send_packet(fd, message, strlen(message), 0,
-                    (struct sockaddr *) &address, sizeof(struct sockaddr_in));
-    check_perror(wc, "send_packet");
-    free(message);
+	int wc = send_packet(fd, message, strlen(message), 0,
+						(struct sockaddr *) &address, sizeof(struct sockaddr_in));
+	check_perror(wc, "send_packet");
+	free(message);
 }
 
 
 void send_client_info(int fd, struct sockaddr_in address, char number, client *current)
 {
-	int message_length = CLIENT_INFO_SIZE + strlen(current->nick) + strlen(current->address) + get_port_length(current->port);
+	int message_length = CLIENT_INFO_SIZE + strlen(current->nick) +
+						 strlen(current->address) + get_port_length(current->port);
 	char *message = malloc(message_length);
-	snprintf(message, message_length, "ACK %c NICK %s IP %s PORT %d", number, current->nick, current->address, current->port);
-    int wc = send_packet(fd, message, strlen(message), 0, (struct sockaddr *) &address, sizeof(struct sockaddr_in));
-    check_perror(wc, "send_packet");
-    free(message);
+	snprintf(message, message_length, "ACK %c NICK %s IP %s PORT %d",
+			 number, current->nick, current->address, current->port);
+
+	int wc = send_packet(fd, message, strlen(message), 0,
+						(struct sockaddr *) &address, sizeof(struct sockaddr_in));
+	check_perror(wc, "send_packet");
+	free(message);
 }
 
 
 void check_perror(int result, char *msg)
 {
-	if (result == -1)
-	{
-		perror(msg);
+    if (result == -1)
+    {
+        perror(msg);
         close(fd);
         free_clients(cache);
         exit(EXIT_FAILURE);
-	}
+    }
 }
 
 
 void check_error(int result, char *msg, int line, const char *function)
 {
-	if (!result)
-	{
-		fprintf(stderr, "> Shutdown triggered by %s in %d on line %s\n", msg, line, function);
+    if (!result)
+    {
+        fprintf(stderr, "> Shutdown triggered by %s in %d on line %s\n", msg, line, function);
         close(fd);
         free_clients(cache);
         exit(EXIT_FAILURE);
-	}
+    }
 }
